59a9c1e8231da6a4935c1b61211e515fac1d0386
Daniel Lucas Soares Ribeiro <daniel.ribeiro@intranet.fip-moc.edu.br>
2020-10-08 11:50:20 -0300

    Iniciando a API, criação do model e interfaces Candidatos

---
+++ API/API/Entities/BancoContext.cs
@@ -0,0 +1,24 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Candidatos> Candidatos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ API/API/Models/Candidatos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Candidatos
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string UrlLinkedin { get; set; }
+        public string UsuarioGithub { get; set; }
+    }
+}
